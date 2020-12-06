2281054dbbac5f39a422f76d527e2fc9bd5f67d7
Thomas Farla <thomas-farla@hotmail.com>
2020-12-06 19:42:13 +0100

    Fix decorations being duplicated in open editors

--- src/extension.ts
+++ src/extension.ts
@@ -2,47 +2,64 @@
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 import compliments from './compliments';
+type OptionMap = { [key: string]: vscode.DecorationOptions };
 
-const decorationType = vscode.window.createTextEditorDecorationType({ after: { margin: '0 0 0 1rem', color: '#7cc36e' } });
+const decorationOptions: vscode.DecorationRenderOptions = { after: { margin: '0 0 0 1rem', color: '#7cc36e' } };
+const editorDecorations: { [uri: string]: { options: OptionMap, type: vscode.TextEditorDecorationType } } = {
+
+};
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-	vscode.window.onDidChangeActiveTextEditor(ev => {
-		showDecorations();
+	vscode.window.onDidChangeActiveTextEditor(() => {
+		vscode.window.visibleTextEditors.forEach(editor => showDecorations(editor));
+	});
+	vscode.workspace.onDidChangeTextDocument(e => {
+		const editor = vscode.window.visibleTextEditors.find((ve) => ve.document.uri === e.document.uri);
+		showDecorations(editor);
 	});
 
-	vscode.workspace.onDidChangeTextDocument(ev => {
-		showDecorations();
-	});
-	const decorations: { [key: string]: vscode.DecorationOptions } = {};
-	function showDecorations() {
-		vscode.window.visibleTextEditors.forEach((e) => {
-			const lines = e.document.getText().split(e.document.eol === vscode.EndOfLine.CRLF ? '\n\r' : '\n');
-			let i = 0;
-			while (i < lines.length) {
-				const line = (lines[i] || '').trim();
-				const key = `${e.document.fileName}.${i}`;
-				if (line.length > 2 && line.startsWith('//')) {
-					const message = getMessage(line);
-					if (!decorations[key]) {
-						decorations[key] = {
-							range: new vscode.Range(new vscode.Position(i, 1024), new vscode.Position(i, 1024)),
-							renderOptions: { after: { contentText: message, fontStyle: 'italic' } }
-						};
-					}
-				} else if (decorations[key]){
-					delete decorations[key];
+	function showDecorations(editor: vscode.TextEditor | undefined) {
+		if (!editor) {
+			return;
+		}
+
+		const lines = editor.document.getText().split(editor.document.eol === vscode.EndOfLine.CRLF ? '\n\r' : '\n');
+		const { path } = editor.document.uri;
+
+		if (!editorDecorations[path]) {
+			editorDecorations[path] = {
+				type: vscode.window.createTextEditorDecorationType(decorationOptions),
+				options: {}
+			};
+		}
+
+		let i = 0;
+		while (i < lines.length) {
+			const line = (lines[i] || '').trim();
+			const key = `${editor.document.uri}.${i}`;
+			if (line.length > 2 && line.startsWith('//')) {
+				const message = getMessage(line);
+				if (!editorDecorations[path].options[key]) {
+					editorDecorations[path].options[key] = {
+						range: new vscode.Range(new vscode.Position(i, 1024), new vscode.Position(i, 1024)),
+						renderOptions: { after: { contentText: message, fontStyle: 'italic' } }
+					};
 				}
-
-				i++;
+			} else {
+				delete editorDecorations[path].options[key];
 			}
-			e.setDecorations(
-				decorationType,
-				Object.values(decorations)
-			);
-		});
+
+			i++;
+		}
+		editor.setDecorations(
+			editorDecorations[path].type,
+			Object.values(editorDecorations[path].options)
+		);
 	}
+
+	vscode.window.visibleTextEditors.forEach(e => showDecorations(e));
 }
 
 // this method is called when your extension is deactivated
@@ -58,6 +75,4 @@ function getMessage(line: string) {
 }
 
 // https://stackoverflow.com/a/5915122
-function randomElement(items: any[]) {
-	return items[Math.floor(Math.random() * items.length)];
-}
+const randomElement = (items: any[]) => items[Math.floor(Math.random() * items.length)];
